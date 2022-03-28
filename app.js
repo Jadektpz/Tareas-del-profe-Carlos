73e5fc5b6b37e6dec1a006055530a36bffa8a126
Jadektpz <johanalva9625@gmail.com>
2022-03-28 15:52:04 -0500

    update

---
+++ app.js
@@ -0,0 +1,123 @@
+
+/* PARTE 1 
+ejercicio 1 */
+
+/* let nombre = "Johan";
+let apellido = "Alva";
+let edad = 25;
+console.log(nombre);
+console.log(apellido);
+console.log(edad); */
+
+/* ejercicio 2 */
+
+/* const ciudadOne = "Lima";
+const ciudadDos = "Trujillo";
+const ciudadTres = "Paracas";
+const ciudadCuatro = "Miraflores";
+const ciudadCinco = "Molina";
+
+console.log(ciudadOne)
+console.log(ciudadDos)
+console.log(ciudadTres)
+console.log(ciudadCuatro)
+console.log(ciudadCinco) */
+
+/* ejercicio 3 */
+
+/* let nombre = "Johan ";
+let apellido = "Alva ";
+let edad = 25;
+let sexo = " Masculino";
+let nacionalidad = " Perú";
+
+let suma = nombre + apellido + edad + sexo + nacionalidad;
+console.log(suma) */
+
+/* ejercicio 4 */
+
+/* let numberOne = prompt ("Ingrese un nombre");
+let numberTwo = prompt("Ingrese otro nombre");
+let numberTrhee = prompt ("Ingrese otro nombre");
+let numberFour = prompt ("Ingrese otro nombre");
+let numberFive = prompt ("Ingrese otro nombre");
+
+let suma = numberOne + numberTwo + numberTrhee + numberFour + numberFive ;
+alert("hola" + suma); */
+
+/* 
+ejercicio 5  */
+
+/* let precioUno = prompt ("ingrese el precio");
+let precioDos = prompt ("ingrese el segundo precio");
+ 
+let resultadoUno = parseInt(precioUno) - parseInt(precioUno)*0.20 ;
+let resultadoDos = parseInt(precioDos) - parseInt(precioDos)*0.30 ;
+alert("el precio con 20% de descuento es :" + resultadoUno);
+alert("el precio con 30% de descuento es:" + resultadoDos); */
+
+
+
+/* PARTE 2 
+
+ejercicio 1 */
+
+/* let nombre = "johan";
+nombre = nombre.toLowerCase();
+
+let usuario = prompt ("ingrese su nombre");
+usuario = usuario.toLowerCase();
+if (usuario == "johan"){
+    alert("fui yo");
+} else {
+    alert ("yo no fui")
+} */
+
+/* ejercicio 2 */
+
+
+/* let boton = prompt ("presione una tecla");
+
+if (boton =="y" || boton == "Y"){
+    alert("correcto");
+} else {
+    alert ("error")
+}
+  */
+
+/* ejercicio 3 */
+
+/* let numero = prompt ("ingrese un numero");
+numero = parseInt(numero);
+
+if (numero == 1){
+    alert("elegiste a Homero");
+} else if (numero == 2){
+    alert("elegiste a Marge");
+} else if(numero == 3){
+    alert("elegiste a Bart");
+} else if(numero == 4){
+    alert("elegiste a Lisa");
+} else{
+    alert("valor incorrecto")
+} */
+
+/* ejercicio 4 */
+
+/* let numero = prompt ("ingrese un numero");
+numero = parseInt(numero);
+
+if(numero>=0 && numero<=1000){
+    alert("presupuesto bajo");
+} else if(numero>=1001 && numero<=3000){
+    alert("presupuesto medio");
+} else if(numero > 3000){
+    alert("presupuesto alto");
+} else{
+    alert("error")
+} */
+
+
+/* RESOLVI LO QUE SE VIO EN LA CLASE , A ESPERAR MAS TEMAS */
+
+
